b4661290602c02cae10cc0e6fff5d923b17a7d57
Dima <dimas.sakulin@yandex.ru>
2020-05-27 18:12:17 +0500

    Начал работу с бэком
    Добавил функцию для работы со шкалами
    Добавил контроллер свайпов

---
+++ SDR/Assets/Scripts/EditBars.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EditBars : MonoBehaviour
+{
+    public Image barHealth, barPsychic, barStudy, barSoc;
+    /// <summary>
+    /// Принимает значения, на которые изменятся шкалы.
+    /// </summary>
+    /// <param name="valueForHealth">float</param>
+    /// <param name="valueForPschic">float</param>
+    /// <param name="valueForStudy">float</param>
+    /// <param name="valueForSoc">float</param>
+    public void EditBar(float valueForHealth, float valueForPschic, float valueForStudy,float valueForSoc)
+    {
+        try
+        {
+            barHealth.GetComponent<RectTransform>().anchorMax += new Vector2(0, valueForHealth);
+            barPsychic.GetComponent<RectTransform>().anchorMax += new Vector2(0, valueForPschic);
+            barStudy.GetComponent<RectTransform>().anchorMax += new Vector2(0, valueForStudy);
+            barSoc.GetComponent<RectTransform>().anchorMax += new Vector2(0, valueForSoc);
+        }
+        catch (System.Exception e) { print(e.Message); }
+        //RectTransform rectTransform = bar.GetComponent<RectTransform>();
+        // rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, 0, 300);
+        //bar.GetComponent<RectTransform>().offsetMax = new Vector2(0, -290);
+    }
+}
